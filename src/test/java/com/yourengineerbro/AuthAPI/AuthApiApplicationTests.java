3cda99a68f4d5813c9827d6160665c6e7412c136
Aman Gupta <guptaaman9981@gmail.com>
2024-07-18 14:40:33 +0530

    Fixed platform compatibility

--- src/test/java/com/yourengineerbro/AuthAPI/AuthApiApplicationTests.java
+++ src/test/java/com/yourengineerbro/AuthAPI/AuthApiApplicationTests.java
@@ -1,9 +1,19 @@
 package com.yourengineerbro.AuthAPI;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(classes = {ServletWebServerFactoryAutoConfiguration.class},
+        webEnvironment = RANDOM_PORT,
+        properties = {"spring.cloud.config.enabled=false"})
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
 class AuthApiApplicationTests {
 
     @Test
